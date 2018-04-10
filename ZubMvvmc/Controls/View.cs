d3d2700c626c2f76403e92664d888e09beb91367
Vince Verwilligen <david.vinke@hotmail.com>
2018-04-10 16:15:00 +0700

    The View has now an empty ViewModel default on it's BindingContext

--- ZubMvvmc/Controls/View.cs
+++ ZubMvvmc/Controls/View.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ZubMvvmc.Core;
 
 namespace ZubMvvmc.Controls
 {
     public class View : Xamarin.Forms.ContentView
     {
+        public View()
+        {
+            BindingContext = new ViewModel();
+        }
     }
 }
