be5b2f9c78081cd781a0c82074d8fefd7c3914ff
Vince Verwilligen <david.vinke@hotmail.com>
2018-04-10 16:19:58 +0700

    NavigationController added for easy acces to the NavigationPage, Show is nowimplemented in the Controller

--- ZubMvvmc/Core/Controller.cs
+++ ZubMvvmc/Core/Controller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using ZubMvvmc.Controls;
 
@@ -7,31 +8,56 @@ namespace ZubMvvmc.Core
 {
     public abstract class Controller
     {
-        public abstract View GetObjectView();
-        public abstract View GetObjectsView();
+        protected abstract View GetObjectView(Model model);
+        protected abstract View GetObjectsView(ObservableCollection<Model> models);
+        //protected abstract Page GetObjectPage();
+        //protected abstract Page GetObjectsPage();
 
         // Add
-        public virtual void Add(Model model)
+        public virtual Model Add(Model model)
         {
-
+            throw new NotImplementedException();
         }
 
         // Show
-        public virtual void Show(Model model)
+        public virtual void Show(Model model, bool modal = false)
         {
+            View objectView = GetObjectView(model);
 
+            if (modal)
+            {
+                NavigationController.Navigation.PushModalAsync(new Xamarin.Forms.ContentPage() { Content = objectView });
+            }
+            else
+            {
+                NavigationController.Navigation.PushAsync(new Xamarin.Forms.ContentPage() { Content = objectView });
+            }
+        }
+
+        public virtual void Show(ObservableCollection<Model> models, bool modal = false)
+        {
+            View objectView = GetObjectsView(models);
+
+            if (modal)
+            {
+                NavigationController.Navigation.PushModalAsync(new Xamarin.Forms.ContentPage() { Content = objectView });
+            }
+            else
+            {
+                NavigationController.Navigation.PushAsync(new Xamarin.Forms.ContentPage() { Content = objectView });
+            }
         }
 
         // Update
-        public virtual void Update(Model model)
+        public virtual Model Update(Model model)
         {
-
+            throw new NotImplementedException();
         }
 
         // Delete
-        public virtual void Delete(Model model)
+        public virtual bool Delete(Model model)
         {
-
+            throw new NotImplementedException();
         }
     }
 }
---
+++ ZubMvvmc/Core/NavigationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace ZubMvvmc.Core
+{
+    public static class NavigationController
+    {
+        public static INavigation Navigation { get; set; }
+
+        static NavigationController()
+        {
+
+        }
+    }
+}
