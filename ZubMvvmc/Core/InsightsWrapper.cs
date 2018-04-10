8375ec8ac0027e1c8e70d1d4647961974d53dbdb
Vince Verwilligen <david.vinke@hotmail.com>
2018-04-10 16:15:52 +0700

    InsightsWrapper added for catching unhandled exceptions and more

---
+++ ZubMvvmc/Core/InsightsWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZubMvvmc.Core
+{
+    public static class InsightsWrapper
+    {
+        public static void ReportException(Exception ex)
+        {
+            string exceptionDescription = ex.Message;
+            string stackTrace = ex.StackTrace;
+        }
+    }
+}
